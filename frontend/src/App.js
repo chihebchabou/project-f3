253139e14ace940ff698339ce4e67c4a19cfb719
chihebchabou <chihebchaboutech@gmail.com>
2023-06-06 10:59:06 +0100

    setup frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <h1 className="text-primary">Hello world</h1>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
